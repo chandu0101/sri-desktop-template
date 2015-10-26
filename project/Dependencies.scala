10844d1b1c5e85dfdcbd151e5eab16a91a0e04a0
chandu0101 <chandu.csu2010@gmail.com>
2015-10-26 10:54:32 +0530

    0.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
 
   val utestVersion = "0.3.0"
 
-   val sriVersion = "0.2.0-SNAPSHOT"
+   val sriVersion = "0.2.0"
 
   val utestJS = libraryDependencies += "com.lihaoyi" %%% "utest" % "0.3.0" % Test
 
